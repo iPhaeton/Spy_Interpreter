c821a79e6eaf1568cbf4838f4dc662706ca05266
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2020-12-05 20:35:57 +0300

    Can handle classes in Interpreter

--- Interpreter.py
+++ Interpreter.py
@@ -2,6 +2,7 @@ from Environmet import Environment
 from Tokenizer import Tokenizer
 from Function import Function
 
+
 class Interpreter:
     def __init__(self):
         self.global_env = Environment(
@@ -19,10 +20,17 @@ class Interpreter:
             ],
         )
 
+    def lhs_eval(self, lhs, env):
+        if isinstance(lhs, list):
+            (_, object_expr, name) = lhs
+            return (self.eval(object_expr, env), name)
+        else:
+            return (env, lhs)
+
     def eval(self, form, env=None):
         if (env == None):
             env = self.global_env
-        
+
         if isinstance(form, int):
             return form
         elif isinstance(form, float):
@@ -34,9 +42,20 @@ class Interpreter:
             for entry in form[1:]:
                 val = self.eval(entry, env)
             return val
+        elif form[0] == 'attr':
+            (_, object_expr, name) = form
+            return self.eval(object_expr, env).lookup(name)
         elif form[0] == 'set':
-            (_, lhs, rhs) = form
-            env.add(lhs, self.eval(rhs, env))
+            (_, lhs, expr) = form
+            (targetEnv, name) = self.lhs_eval(lhs, env)
+            targetEnv.add(name, self.eval(expr, env))
+        elif form[0] == 'class':
+            (_, name, super, body) = form
+            if super == 'None':
+                super = self.global_env
+            class_env = Environment(parent=super)
+            env.add(name, lambda: Environment(parent=class_env))
+            self.eval(body, class_env)
         elif form[0] == 'def':
             (_, name, params, body) = form
             env.add(name, Function(params, body, env))
@@ -50,7 +69,11 @@ class Interpreter:
             f = self.eval(form[0], env)
 
             if callable(f):
-                return f(*[self.eval(x, env) for x in form[1:]])
+                if (len(form[1:]) == 1 and form[1:][0] == 'None'):
+                    return f()
+                else:
+                    args = [self.eval(x, env) for x in form[1:]]
+                    return f(*args)
             else:
                 return self.eval(f.body,
                                  Environment(
@@ -61,15 +84,33 @@ class Interpreter:
         else:
             raise "Illegal expression: " + str(form)
 
+        # print(env.dictionary)
+
 
 text1 = '''
     (begin
         (set x 5)
         (def sum (a b) (if (>= a b) (+ (+ a b) x) 0))
-        (sum 1 2)
+        (sum 7 2)
     )
 '''
-tokenizer = Tokenizer(text1)
+
+text2 = '''
+    (begin
+        (set x 0)
+        (class Class None
+            (begin
+                (set a 1)
+                (def get_x () x)
+                (def get_a () a)
+            )
+        )
+        (set cl (Class None))
+        ((attr cl get_a) cl)
+    )
+'''
+
+tokenizer = Tokenizer(text2)
 expression1 = tokenizer.tokenize()
 print(expression1)
 interpreter = Interpreter()
